4d98280326eea326231fc45499d0a98d7cc90df7
Joe Longman <joelongman8@gmail.com>
2021-02-12 11:42:34 +0000

    refactored out dog responses to a message handler

---
+++ option6/helpers/messagePublisher.py
@@ -0,0 +1,10 @@
+class messagePublisher():
+    """simple publisher to send messages to observers"""
+    _messageHandlers = []
+
+    def subscribe(self, handler):
+        self._messageHandlers.append(handler)
+    
+    async def publish(self, message):
+        for handler in self._messageHandlers:
+            await handler.respond(message)
