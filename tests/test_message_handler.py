6e52c8ac4e81b72115f13f81fa76d116d5b8b1c5
Edward Knight <edw@rdknig.ht>
2021-03-14 13:08:39 +0000

    Add a quick test for the PhraseMessageHandler

--- tests/test_message_handler.py
+++ tests/test_message_handler.py
@@ -1,7 +1,12 @@
 """Tests for option6.helpers.message_handler."""
 import pytest
 
-from option6.helpers.message_handler import MessageHandler
+from option6.helpers.message_handler import MessageHandler, PhraseMessageHandler
+
+
+class _TestMessage:
+    def __init__(self, message: str):
+        self.content = message
 
 
 @pytest.mark.parametrize(
@@ -20,10 +25,29 @@ def test_word_recognition(message: str, should_respond: bool):
         async def handle(self, _message) -> None:
             pass
 
-    class TestMessage:
-        content = message
-
-    fake_message = TestMessage()
+    fake_message = _TestMessage(message)
     handler = TestMessageHandler()
+
+    assert handler.should_respond(fake_message) == should_respond
+
+
+@pytest.mark.parametrize(
+    "message, should_respond",
+    [
+        ("test", True),
+        ("TEST", True),
+        ("Match on test.", True),
+        ("intestine", True),
+    ],
+)
+def test_phrase_recognition(message: str, should_respond: bool):
+    class TestPhraseHandler(PhraseMessageHandler):
+        keywords = {"test"}
+
+        async def handle(self, _message) -> None:
+            pass
+
+    fake_message = _TestMessage(message)
+    handler = TestPhraseHandler()
 
     assert handler.should_respond(fake_message) == should_respond
