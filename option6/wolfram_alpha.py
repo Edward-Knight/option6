e7f39afeceab61e6f403bc34693d03672c16cba8
Edward Knight <edw@rdknig.ht>
2021-04-02 23:51:37 +0100

    Add /ask command backed by Wolfram|Alpha

---
+++ option6/wolfram_alpha.py
@@ -0,0 +1,18 @@
+"""Functions for interacting with Wolfram|Alpha."""
+import functools
+
+from wolframalpha import Client
+
+from option6 import KEYS
+
+
+@functools.lru_cache(None)
+def get_client() -> Client:
+    """Return a Wolfram|Alpha client singleton."""
+    return Client(KEYS["wolfram"])
+
+
+def query(q: str) -> str:
+    """Query Wolfram|Alpha."""
+    client = get_client()
+    return next(client.query(q).results).text
