9df63fa264179d7e199adadb6db1578310d1bdc8
Edward Knight <edw@rdknig.ht>
2021-06-11 22:28:01 +0100

    Add test for update_keys

--- tests/test_misc.py
+++ tests/test_misc.py
@@ -1,11 +1,20 @@
 """Test miscellaneous functions in option6/__init__.py."""
+import io
 import re
 import subprocess
 from pathlib import Path
 
 import pytest
 
-from option6 import __version__, git_hash, update_and_reinstall, version_on_disk
+import option6
+from option6 import __version__, git_hash, update_and_reinstall, update_keys, version_on_disk
+
+
+def test_update_keys():
+    f = io.StringIO('{"test_key": "test_value"}')
+    assert "test_key" not in option6.KEYS
+    update_keys(f)
+    assert option6.KEYS["test_key"] == "test_value"
 
 
 def test_git_hash(git_repo):
